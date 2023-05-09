c8f03c8075d1b496b1999700816469ed17d7a672
monikachn <monika.chn@gmail.com>
2023-05-09 22:38:45 +0100

    nopcommerce sw5

---
+++ src/test/java/com/nopcommerce/demo/testsuite/TopMenuTest.java
@@ -0,0 +1,36 @@
+package com.nopcommerce.demo.testsuite;
+import com.nopcommerce.demo.customlisteners.CustomListeners;
+import com.nopcommerce.demo.pages.ComputerPage;
+import com.nopcommerce.demo.pages.HomePage;
+import com.nopcommerce.demo.testbase.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(CustomListeners.class)
+
+public class TopMenuTest extends BaseTest {
+    HomePage homePage;
+    ComputerPage computerPage;
+
+    @BeforeMethod(alwaysRun = true)
+    public void inIt() {
+        homePage = new HomePage();
+        computerPage = new ComputerPage();
+    }
+
+    @Test(groups = {"sanity", "regression"})
+    public void verifyPageNavigation() {
+//        1. create class "TopMenuTest"
+//        1.1 create method with name "selectMenu" it has one parameter name "menu" of type
+//        string
+//        1.2 This method should click on the menu whatever name is passed as parameter.
+        homePage.selectMenu("Computers");
+//        1.3. create the @Test method name verifyPageNavigation.use selectMenu method to
+//        select the Menu and click on it and verify the page navigation
+        String expectedMessage = "Computers";
+        String actualMessage = computerPage.verifyComputerText();
+        Assert.assertEquals(actualMessage, expectedMessage, "Computers page not display");
+    }
+}
